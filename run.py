0621158360d412c9f781d148580744393dcb55d8
arwaalanqary <alanqary@mit.edu>
2021-01-04 22:23:27 -0500

    new miscrosoft params

--- run.py
+++ run.py
@@ -9,7 +9,7 @@ from evaluation.classification import compute_f1_score
 from algorithms.utils import data_split, save_results_json, save_results_table
 
 #Specify experiment
-algorithm_name = 'binseg'
+algorithm_name = 'microsoft_ssa'
 dataset = 'yahoo'
 data_names = DATASETS[dataset] ##All data files in the dataset
 metric = 'compute_f1_score'
